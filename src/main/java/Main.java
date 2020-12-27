acf40a1ec67845ca941f9d0090b094ed16c8a34a
Mai Huy Thong <1712800@student.hcmus.edu.vn>
2020-12-27 12:16:42 +0700

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.validation.Validation;
+
+public class Main {
+    public static void main(String[] args) {
+        Validation validation = new Validation();
+        validation.test();
+    }
+}
---
+++ src/main/java/com/validation/Validation.java
@@ -0,0 +1,7 @@
+package com.validation;
+
+public class Validation {
+    public void test(){
+        System.out.println("Hello");
+    }
+}
